04572407ef34269382ad2cad575349c50c6f658b
sowidvhu <sowidv@hu-berlin.de>
2016-04-29 11:20:05 +0200

    Update chocolateyInstall.ps1

--- seafile-client-shibboleth/tools/chocolateyInstall.ps1
+++ seafile-client-shibboleth/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'seafile-client-shibboleth'
 $fileType = 'msi'
 $silentArgs = '/passive'
-$url = 'https://bintray.com/artifact/download/seafile-org/seafile/seafile-4.2.3-en-shibboleth.msi'
+$url = 'https://bintray.com/artifact/download/seafile-org/seafile/seafile-5.1.0-en-shibboleth.msi'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
