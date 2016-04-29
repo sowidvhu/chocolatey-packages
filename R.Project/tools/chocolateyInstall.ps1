d59d36566b9a45eebbdd92b617a6879114976f10
sowidvhu <goroncan@sowi.hu-berlin.de>
2016-04-29 11:14:08 +0200

    remove r.project

--- R.Project/tools/chocolateyInstall.ps1
+++
@@ -1,2 +0,0 @@
-﻿$url='http://cran.rstudio.com/bin/windows/base/R-2.15.2-win.exe'
-Install-ChocolateyPackage 'R.Project' 'exe' '/silent' "$url" -validExitCodes @(0)
--- R.Project/tools/chocolateyUninstall.ps1
+++
@@ -1,2 +0,0 @@
-﻿$unpath="${Env:ProgramFiles}\R\R-2.15.2\unins000.exe"
-Uninstall-ChocolateyPackage 'R.Project' 'exe' '/silent' "$unpath" -validExitCodes @(0)
