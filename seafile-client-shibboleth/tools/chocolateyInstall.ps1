23ba97df1fb6850bf2760b7f449ad7679ed2009c
sowidvhu <sowidv@hu-berlin.de>
2017-05-11 14:24:03 +0200

    Version 6.0.6

--- seafile-client-shibboleth/tools/chocolateyInstall.ps1
+++ seafile-client-shibboleth/tools/chocolateyInstall.ps1
@@ -1,6 +1,9 @@
 ﻿$packageName = 'seafile-client-shibboleth'
 $fileType = 'msi'
 $silentArgs = '/quiet /norestart'
-$url = 'https://bintray.com/artifact/download/seafile-org/seafile/seafile-5.1.1-en-shibboleth.msi'
+$url = 'https://bintray.com/artifact/download/seafile-org/seafile/seafile-6.0.6-en-shibboleth.msi'
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+$checksum = '729E1F44703020481A3278DBB456828A02BEA151D645DADF66C161FB9A27BADF'
+$checksumType = 'sha256'
+
+Install-ChocolateyPackage $packageName $fileType $silentArgs $url -CheckSum $checksum -ChecksumType $checksumType
