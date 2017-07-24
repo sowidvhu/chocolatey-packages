29a781994c7599134f7ccc60c2b38f86b74ddb90
Andreas Goroncy <goroncan@sowi.hu-berlin.de>
2017-07-24 10:11:40 +0200

    0.153

--- R.Studio/tools/chocolateyInstall.ps1
+++ R.Studio/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-﻿$version = '0.99'
-$minor = '902'
-Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "http://download1.rstudio.org/RStudio-$version.$minor.exe"  -validExitCodes @(0)
+﻿$checksum = '06AA4DD1CF73F689FDD80BF6639B1B6EB34D9E6B4A3989D0999FEAE200372A11'
+$checksumType = 'sha256'
+Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/RStudio-1.0.153.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
