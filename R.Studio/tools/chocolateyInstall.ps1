5fe8119c8e68e8b261f438b448e41bc34623a8dc
sowidvhu <sowidv@hu-berlin.de>
2017-07-24 10:19:04 +0200

    Update chocolateyInstall.ps1

--- R.Studio/tools/chocolateyInstall.ps1
+++ R.Studio/tools/chocolateyInstall.ps1
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
 ﻿$checksum = '06AA4DD1CF73F689FDD80BF6639B1B6EB34D9E6B4A3989D0999FEAE200372A11'
 $checksumType = 'sha256'
 Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/RStudio-1.0.153.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
-=======
-<<<<<<< HEAD
-﻿$checksum = 'A778348D9E58D3466BCF60C256F6F4C04BC6E78DDB73775332F3EBE3D8EA775C'
-$checksumType = 'sha256'
-Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/RStudio-1.0.143.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
-=======
-$checksum = 'A778348D9E58D3466BCF60C256F6F4C04BC6E78DDB73775332F3EBE3D8EA775C'
-$checksumType = 'sha256'
-Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/RStudio-1.0.143.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
->>>>>>> 13459fddbc7f46ee7eabc49e94d212b47b8c39aa
->>>>>>> 23ba97df1fb6850bf2760b7f449ad7679ed2009c
