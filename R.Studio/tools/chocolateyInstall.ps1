788aa76cbdbc498d4e372f190cf70044d95618f9
Fabio A. Correa Duran <facorread@gmail.com>
2019-11-11 11:38:07 -0500

    R.Studio.1.2.5019

--- R.Studio/tools/chocolateyInstall.ps1
+++ R.Studio/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-﻿$checksum = 'c54d8779f363ec9636c7831e577521bd'
+﻿$checksum = '4d6521a9b89d70c3bf50414c8b6708f2'
 $checksumType = 'md5'
-Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/desktop/windows/RStudio-1.2.5001.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
+Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/desktop/windows/RStudio-1.2.5019.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
