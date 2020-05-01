b5ce5bd6ff44df43589c4416a786af5b3c2175b2
Fabio A. Correa Duran <facorread@gmail.com>
2020-05-01 08:29:43 -0400

    RStudio-1.2.5042

--- R.Studio/tools/chocolateyInstall.ps1
+++ R.Studio/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-﻿$checksum = '7fd3bc1b7464ec0d69e1d88b12f3e824684a507b6431927441138986cda59226'
+﻿$checksum = '5d4cd64426ee0d9ad520861c86ea11783c16b0daff46006f75b1c2f50ae78f34'
 $checksumType = 'sha256'
-Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/desktop/windows/RStudio-1.2.5033.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
+Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "https://download1.rstudio.org/desktop/windows/RStudio-1.2.5042.exe" -CheckSum $checksum -ChecksumType $checksumType -validExitCodes @(0)
